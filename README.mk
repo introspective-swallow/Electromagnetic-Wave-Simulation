43e7e9921d85e7f34bb6a3e1a743844ec109aa47
Guillem <guillem911@gmail.com>
2024-09-25 12:10:40 +0200

    First commit with simple tests.

---
+++ README.mk
@@ -0,0 +1,7 @@
+Implementation of various electromagnetic wave simulations in Python using the package Meep for FTDT methods.
+
+## Use
+Output a list of pngs with mp.run and convert to gif using
+```
+convert [file-name]-ez*.png ez.gif
+```
---
+++ test2.py
@@ -0,0 +1,32 @@
+import meep as mp
+
+cell = mp.Vector3(16,16,0)
+geometry = [mp.Block(mp.Vector3(12,1,mp.inf),
+                     center=mp.Vector3(-2.5,-3.5),
+                     material=mp.Medium(epsilon=12)),
+            mp.Block(mp.Vector3(1,12,mp.inf),
+                     center=mp.Vector3(3.5,2),
+                     material=mp.Medium(epsilon=12))]
+pml_layers = [mp.PML(1.0)]
+resolution = 10
+
+sources = [mp.Source(mp.ContinuousSource(wavelength=2*(11**0.5), width=20),
+                     component=mp.Ez,
+                     center=mp.Vector3(-7,-3.5),
+                     size=mp.Vector3(0,1))]
+
+
+sim = mp.Simulation(cell_size=cell,
+                    boundary_layers=pml_layers,
+                    geometry=geometry,
+                    sources=sources,
+                    resolution=resolution)
+
+vals = []
+
+def get_slice(sim):
+    vals.append(sim.get_array(center=mp.Vector3(0,-3.5), size=mp.Vector3(16,0), component=mp.Ez))
+
+
+sim.use_output_directory()
+sim.run(mp.at_every(0.6 , mp.output_png(mp.Ez, "-Zc dkbluered")), until=200)
